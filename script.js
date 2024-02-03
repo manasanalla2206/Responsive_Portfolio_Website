35032ea19027fb9939ae4aa70a43480b6ba501e5
manasanalla2206 <nmanasa2206@gmail.com>
2024-02-03 22:37:00 +0530

    Finished website

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+          const menu=document.querySelector(".menu-links");
+          const icon=document.querySelector(".manasa-icon");
+          menu.classList.toggle("open");
+          icon.classListtoggle("open");
+}
